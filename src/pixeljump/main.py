30d14b9ca12ec336dd24e5e51e6111a0dbc99468
Wilson Oh <e0773510@u.nus.edu>
2022-07-10 23:17:35 +0800

    add window icon

--- src/pixeljump/main.py
+++ src/pixeljump/main.py
@@ -5,6 +5,7 @@ from pixeljump.levels.act1 import ActOne
 from pixeljump.levels.act2 import ActTwo
 from pixeljump.menu import show_menu
 from pixeljump.settings import load_settings
+from pixeljump.assets import get_sprite_image
 
 import pygame
 
@@ -21,6 +22,7 @@ def main():
     pygame.mixer.pre_init(44100, -16, 2, 512)
     pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("PIXELJUMP")
+    pygame.display.set_icon(get_sprite_image("KNIGHT", (32, 32)))
     act = show_menu()
     if act == 0 or act == 1:
         ActOne().run()
